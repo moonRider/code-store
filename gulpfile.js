416efbf629b81a9a64ef124ef980e0fdc739d97f
moonRider <solar.pom@gmail.com>
2015-06-10 15:14:12 +0800

    Fix: added file event not be watched and watch task stoped by error

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var config = require('./config.json');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
 
 gulp.task('sass', function(){
   return gulp
-    .src(config.sass.sourceDir + config.sass.compile, {base: './assets'})
+    .src(config.sass.sourceDir + config.sass.compile, {base: 'assets'})
+    .pipe(plumber())
     .pipe(sass(config.sass.options))
     .pipe(gulp.dest(config.sass.dest));
 });
