cfa496358123be3b344ae831a9e97e8ec1febf09
moonRider <solar.pom@gmail.com>
2015-06-11 03:53:52 +0800

    Add: coffee compile task and watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var config = require('./config.json');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var coffee = require('gulp-coffee');
 var plumber = require('gulp-plumber');
 
 gulp.task('sass', function(){
@@ -11,6 +12,19 @@ gulp.task('sass', function(){
     .pipe(gulp.dest(config.sass.dest));
 });
 
-gulp.task('watch', function(){
-  gulp.watch(config.sass.sourceDir + config.sass.watch, ['sass']);
+gulp.task('coffee', function(){
+  return gulp
+    .src(config.coffee.sourceDir + config.coffee.compile, {base: 'assets'})
+    .pipe(plumber())
+    .pipe(coffee(config.coffee.options))
+    .pipe(gulp.dest(config.coffee.dest));
 });
+
+gulp.task('watch', function(){
+  // watch sass file
+  gulp.watch(config.sass.sourceDir + config.sass.watch, ['sass']);
+  // watch coffee file
+  gulp.watch(config.coffee.sourceDir + config.coffee.watch, ['coffee']);
+});
+
+gulp.task('default', ['sass', 'coffee', 'watch']);
