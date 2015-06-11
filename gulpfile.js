21b3839b705249b66199fb8f17049b0acea4848b
moonRider <solar.pom@gmail.com>
2015-06-11 23:29:44 +0800

    Add: mocha test to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
+require('coffee-script/register');
 var config = require('./config.json');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var coffee = require('gulp-coffee');
 var plumber = require('gulp-plumber');
+var mocha = require('gulp-mocha');
 
 gulp.task('sass', function(){
   return gulp
@@ -20,11 +22,19 @@ gulp.task('coffee', function(){
     .pipe(gulp.dest(config.coffee.dest));
 });
 
+gulp.task('mocha', function(){
+  return gulp
+    .src(config.test.sourceDir + config.test.files, {read: false})
+    .pipe(mocha(config.test.options));
+});
+
 gulp.task('watch', function(){
   // watch sass file
   gulp.watch(config.sass.sourceDir + config.sass.watch, ['sass']);
   // watch coffee file
   gulp.watch(config.coffee.sourceDir + config.coffee.watch, ['coffee']);
+  // watch test files
+  gulp.watch(config.test.sourceDir + config.test.files, ['mocha']);
 });
 
 gulp.task('default', ['sass', 'coffee', 'watch']);
